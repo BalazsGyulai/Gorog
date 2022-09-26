616f358936fc2696bbd47569d4774522c27a891c
kecifej <67418021+kecifej@users.noreply.github.com>
2022-09-26 07:25:49 -0700

    Add files via upload

---
+++ results.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Results</title>
+</head>
+<body>
+
+    <div class="results" style="text-align:center" >
+        <h1>Results</h1>
+        <br>
+        <h1>Yes</h1><h3 id="yes">0</h3>
+        <br>
+        <h1>No</h1> <h3 id="no">0</h3>
+        <br>
+        <h1>I don't know</h1> <h3 id="idk">0</h3>
+        <br>
+
+        
+    </div>
+    
+</body>
+</html>
