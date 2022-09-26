e50f4f4e864e4966bbd1119e0016a0f87ae9c37f
kecifej <67418021+kecifej@users.noreply.github.com>
2022-09-26 06:45:17 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Trainer</title>
+</head>
+<body>
+    <font color="blue"></font>
+    <div class="form">
+    <form>
+        <h2>Question</h2>
+        
+        
+        <input type="text" id="question" name="question">
+        <input type="checkbox" name="publish" id="publishq" value="publish">
+        <label for="publishq">Publish</label>
+        <br>
+        <br>
+        
+        <input type="submit" value="Yes">
+        <input type="submit" value="No">
+        <input type="submit" value="I don't know">
+        <br>
+        <br>
 
-echo "heello world"
+        <input type="submit" value="Save">
 
-?>
+      </form>
+      </div>
+</body>
+</html>
