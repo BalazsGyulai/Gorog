a55009ba4d63cf7429f37635fda257b12af9687a
kecifej <67418021+kecifej@users.noreply.github.com>
2022-09-27 06:11:54 -0700

    Update js.js

--- js.js
+++ js.js
@@ -1,49 +1,28 @@
-function yes2()
+function ido()
 {
     let currentDate=new Date();
     let time = currentDate.getHours()+":"+currentDate.getMinutes()+":"+currentDate.getSeconds();
     
-    
-   
+    if(document.getElementById("yes1").checked)
+    {
         var yes = document.getElementById("yes1").value=time;
-   
-    
-    console.log("yes"+yes)
-    
 
-
-
-}
-function no2()
-{
-    let currentDate=new Date();
-    let time = currentDate.getHours()+":"+currentDate.getMinutes()+":"+currentDate.getSeconds();
+    }
     
-   
-   
+    if(document.getElementById("no1").checked)
+    {
         var no = document.getElementById("no1").value=time;
-   
-    
-    console.log("no"+no)
-    
 
-
-
-}
-function idk2()
-{
-    let currentDate=new Date();
-    let time = currentDate.getHours()+":"+currentDate.getMinutes()+":"+currentDate.getSeconds();
+    }
     
-    
-   
+    if(document.getElementById("idk1").checked)
+    {
         var idk = document.getElementById("idk1").value=time;
-   
+    }
     
-    console.log("idk"+idk)
+   
     
 
 
 
 }
-
