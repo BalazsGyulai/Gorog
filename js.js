b5693988a429c83bc695f86e54a5a746498d9fd4
kecifej <67418021+kecifej@users.noreply.github.com>
2022-09-27 05:52:12 -0700

    Update js.js

--- js.js
+++ js.js
@@ -1,28 +1,49 @@
-function ido()
+function yes2()
 {
     let currentDate=new Date();
     let time = currentDate.getHours()+":"+currentDate.getMinutes()+":"+currentDate.getSeconds();
     
-    if(document.getElementById("yes1").checked)
-    {
+    
+   
         var yes = document.getElementById("yes1").value=time;
-
-    }
+   
     
-    else if(document.getElementById("no1").checked)
-    {
-        var no = document.getElementById("no1").value=time;
-
-    }
-    
-    else if(document.getElementById("idk1").checked)
-    {
-        var idk = document.getElementById("idk1").value=time;
-    }
-    
-    console.log("yes"+yes+" "+"no"+no+" "+"idk"+idk)
+    console.log("yes"+yes)
     
 
 
 
 }
+function no2()
+{
+    let currentDate=new Date();
+    let time = currentDate.getHours()+":"+currentDate.getMinutes()+":"+currentDate.getSeconds();
+    
+   
+   
+        var no = document.getElementById("no1").value=time;
+   
+    
+    console.log("no"+no)
+    
+
+
+
+}
+function idk2()
+{
+    let currentDate=new Date();
+    let time = currentDate.getHours()+":"+currentDate.getMinutes()+":"+currentDate.getSeconds();
+    
+    
+   
+        var idk = document.getElementById("idk1").value=time;
+   
+    
+    console.log("idk"+idk)
+    
+
+
+
+}
+
