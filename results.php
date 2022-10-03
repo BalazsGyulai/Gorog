429069de12b5ae4775dd67ed5c97d622f5d528f6
BalazsGyulai <trombita0531@gmail.com>
2022-10-02 23:55:44 -0700

    maybe working

--- results.php
+++ results.php
@@ -56,13 +56,49 @@ if ($result->num_rows > 0){
         <br>
         <h1>I don't know</h1> <h3 id="idk"><?php echo $row2["dont_know"] ?></h3>
         <br>
-
-        
+        <div style="width: 40%;">
+        <canvas id="myChart"></canvas>
+</div>
     </div>
 </div>
 </div>
 </div>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js" integrity="sha512-ElRFoEQdI5Ht6kZvyzXhYG9NqjtkmlkfYk0wr6wHxU9JEHakS7UJZNeml5ALk+8IKlU6jDgMabC3vkumRokgJA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+const ctx = document.getElementById('myChart').getContext('2d');
+let yes = <?php echo $row2["yes"] ?>;
+let no = <?php echo $row2["no"] ?>;
+let dontknow = <?php echo $row2["dont_know"] ?>;
+const myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Yes', 'No', "I don't know"],
+        datasets: [{
+            label: '# of Votes',
+            data: [yes, no, dontknow],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+</script>
     
 </body>
 </html>
